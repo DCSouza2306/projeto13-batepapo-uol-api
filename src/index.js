4f319bf73ab59f663f8f743e9a6c388bf6be5eea
Diego Candido <diegocan.sou@gmail.com>
2022-11-09 22:41:56 -0300

    Banco de dados Mongo conectado

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@ import { MongoClient } from "mongodb";
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db("batePapoUol")
+})
+
+
 app.listen(5000, () => {console.log("Server running in port 5000")});
